55986fb8e08e4b898e999fa510f7af7a9f8e3fa4
Ken Okada <kokada60@gmail.com>
2025-09-02 10:37:51 -0700

    added one json sample snippet

---
+++ test1.sh
@@ -0,0 +1,10 @@
+read -r -d '' json1 <<EOF
+[
+    {"id": 1, "name": "Ken", "role": "multi-role", "dob": "2017-01-01"},
+    {"id": 2, "name": "Kenneth", "role": "developer", "dob": "2012-06-07"},
+    {"id": 3, "name": "Kenni", "role": "vice-president", "dob": "2014-11-20"}, 
+    {"id": 4, "name": "Kenny", "role": "director", "dob": "2001-02-27"}, 
+    {"id": 5, "name": "Kenyon", "role": "manager", "dob": "2005-09-15", "b": 100}
+]
+EOF
+
